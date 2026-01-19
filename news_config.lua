d7217ad3e37582eaa32194c608879fb203b1954e
jodiardianboy-code <jodiardianboy@gmail.com>
2026-01-19 08:56:58 +0700

    Update news_config.lua

--- news_config.lua
+++ news_config.lua
@@ -4,16 +4,22 @@
     TITLE = "🎮 Script MultiGame DRABOYGAMING™ 🇮🇩",
     MESSAGE = [[
    
-    
+    ⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
     📢  CHANGELOG / UPDATE:
     ⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
+
+    
     🚇 Metro 2050 Survival
         • 🔥 Fix something
         • 🔥 Add New Mod Unlimited Gold
+
+
+    
     ⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
     
-    Follow Ytb @DraboyGaming
-    Group Telegram @DraboyGaming
+    ♥️ Follow Ytb @DraboyGaming
+    ♥️ Group Telegram @DraboyGaming
+    
     ]],
     LAST_UPDATE = "19-01-2026"
 }
