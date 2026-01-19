4ff3df8d1d15c2c4acf51487093b40e92ee9d444
jodiardianboy-code <jodiardianboy@gmail.com>
2026-01-19 08:37:16 +0700

    Add files via upload

---
+++ games_config.lua
@@ -0,0 +1,36 @@
+{
+    GAMES = {
+        {
+            name = " ╰▶ 🎃 Grim Soul: Dark Fantasy",
+            version = "7.9.0",
+            link = "https://raw.githubusercontent.com/jodiardianboy-code/Menu_Loader/refs/heads/main/grimsoul.lua"
+        },
+        {
+            name = " ╰▶ 🧟 Last Day on Earth: Survival",
+            version = "1.43.0",
+            link = "https://raw.githubusercontent.com/jodiardianboy-code/Menu_Loader/refs/heads/main/ldoe.lua"
+        },
+        {
+            name = " ╰▶ 🚇 Metro 2050 Survival",
+            version = "0.0.365",
+            link = " https://raw.githubusercontent.com/jodiardianboy-code/Menu_Loader/refs/heads/main/metrov2.lua"
+           
+        },
+        {
+            name = " ╰▶ 👹 Dead God Land [Tested]",
+            version = "0.0.0349",
+            link = "https://raw.githubusercontent.com/jodiardianboy-code/Menu_Loader/refs/heads/main/deadgodland.lua"
+        },
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ news_config.lua
@@ -0,0 +1,19 @@
+{
+    
+   
+    TITLE = "🔥 DRABOY NEWS 🔥",
+    MESSAGE = [[
+Welcome to Draboy Cloud Loader
+    
+📢  CHANGELOG / UPDATE:
+    
+   🚇 Metro 2050 Survival
+        • 🔥 Fix something
+        • 🔥 Add New Mod Unlimited Gold
+    
+
+
+Follow @DraboyGaming
+    ]],
+    LAST_UPDATE = "12-01-2026"
+}
